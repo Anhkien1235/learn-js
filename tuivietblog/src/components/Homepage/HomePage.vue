f9ae28478a281f52ada9d0ce35863f8ce1cab715
unknown <ddaamsnhau1999@gmail.com>
2024-01-23 00:36:43 +0700

    Cài tailwind , mai tiep tuc cai

---
+++ tuivietblog/src/components/Homepage/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+    
+        <button class="bg-cyan-500 hover:bg-cyan-600 ">Subscribe</button>
+    
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomePage',
+  }
+  </script>
+  
+  <style scoped>
+  </style>
