7d5d38bb5b6d28d17f38b3fedce83a4dd1ae7797
Kudo ShiniKien <trungkiennn3@gmail.com>
2024-01-26 00:36:44 +0700

    update tiếp nè . mai làm menu

--- tuivietblog/src/App.vue
+++ tuivietblog/src/App.vue
@@ -1,13 +1,19 @@
-<script setup>
+<script setup lang="ts">
 </script>
 
-<template>
-  
-
-  <div class="text-3xl font-bold underline text-red-700">
-    tui viet blog
+<template >
+  <div class="text-3xl font-bold underline bg-teal-400 ">
+Tui Viet Blog
   </div>
+  <div class="text-3xl font-bold underline bg-teal-400 ">
+Tui Viet Blog
+  </div>
+  <div class="text-3xl font-bold underline bg-teal-400 ">
+Tui Viet Blog
+  </div>
+  <span class="text-2xl ">Kiennnt</span>
 </template>
 
 <style scoped>
+
 </style>
